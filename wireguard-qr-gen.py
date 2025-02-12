ddd2fbf8ae816eae9850a68033be22afcef3ab16
Κωνσταντίνος Καρακασίδης <defendergr@gmail.com>
2025-02-12 18:01:42 +0200

    first commit

---
+++ wireguard-qr-gen.py
@@ -0,0 +1,16 @@
+import qrcode
+
+with open("wireguard.conf", "r") as f:
+    wg = f.read()
+    
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=4,
+        border=2,
+    )
+
+    qr.add_data(wg)
+    qr.make(fit=True)
+    qr.print_ascii(tty=True)
+    qr.make_image(fill_color="black", back_color="white").save("wireguard.png")
